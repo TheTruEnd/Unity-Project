f12227a01286821b77358b66db46a27fd269a5ab
TheTruEnd <ntdat1602@gmail.com>
2024-01-09 20:59:28 +0700

    Create Bullet

--- Assets/_FlappyBird/Sripts/Player.cs
+++ Assets/_FlappyBird/Sripts/Player.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using GameTool;
+using Unity.Mathematics;
+using UnityEditor.U2D.Sprites;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    public GameObject prifab;
+    public float cooldown;
+    public float timeShoot = 0.5f;
 
     private float jumpForce = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(new Vector2(0, 20));
+        cooldown = timeShoot;
         //rb.velocity = new Vector2(0, 20);
     }
 
@@ -22,13 +32,23 @@ public class Player : MonoBehaviour
         {
             rb.velocity = new Vector2(0, jumpForce);
         }
-    }
 
+        cooldown -= Time.deltaTime;
+        if (cooldown <= 0.0f)
+        {
+            cooldown = timeShoot;
+            PoolingManager.Instance.GetObject(NamePrefabPool.Bullet, parent:null, transform.position).Disable(1);
+        }
+        
+    }
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.transform.CompareTag("Block"))
         {
             Debug.Log("Collide with Block");
+            Instantiate(prifab, new Vector3(0f, 0f), quaternion.identity);
+            Destroy(other.gameObject);
         }
     }
 }
